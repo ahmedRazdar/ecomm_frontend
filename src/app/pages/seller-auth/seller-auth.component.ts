c2b669406c8cd8b3dd5569b07cc047cc7becc1a3
AHMED-RAZA <ahmedraza.razdar@gmail.com>
2023-06-05 18:08:58 +0500

    by ahmed

--- src/app/pages/seller-auth/seller-auth.component.ts
+++ src/app/pages/seller-auth/seller-auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SellerService } from 'src/app/services/SellerServices/seller.service';
 
 @Component({
   selector: 'app-seller-auth',
@@ -7,8 +8,11 @@ import { Component } from '@angular/core';
 })
 export class SellerAuthComponent {
 
+  constructor(private seller:SellerService){}
+
   signUp(data:object):void{
     console.warn(data);
+    // this.seller.userSignUp();
   }
 
 }

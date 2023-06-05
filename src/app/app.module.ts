68c56e64e9e4b8e0cfa6b4b98d02bc470f23ca5b
AHMED-RAZA <ahmedraza.razdar@gmail.com>
2023-06-05 16:47:54 +0500

    by ahmed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SellerAuthComponent } from './pages/seller-auth/seller-auth.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SellerAuthComponent } from './pages/seller-auth/seller-auth.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
